89e4d21c39960e78dea5aeb244dd6e372217232c
LEVIN-CN <40206656+LEVIN-CN@users.noreply.github.com>
2018-06-12 22:04:07 +0800

    Add files via upload

---
+++ src/main/java/local/vo/HBall.java
@@ -0,0 +1,515 @@
+/**
+ * 
+ */
+package local.vo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HBall {
+
+	private int r_1;
+	private int r_2;
+	private int r_3;
+	private int r_4;
+	private int r_5;
+	private int r_6;
+	private int r_7;
+	private int r_8;
+	private int r_9;
+	private int r_10;
+	private int r_11;
+	private int r_12;
+	private int r_13;
+	private int r_14;
+	private int r_15;
+	private int r_16;
+	private int r_17;
+	private int r_18;
+	private int r_19;
+	private int r_20;
+	private int r_21;
+	private int r_22;
+	private int r_23;
+	private int r_24;
+	private int r_25;
+	private int r_26;
+	private int r_27;
+	private int r_28;
+	private int r_29;
+	private int r_30;
+	private int r_31;
+	private int r_32;
+	private int r_33;
+	
+	/**
+	 * 
+	 */
+	
+	private int b_1;
+	private int b_2;
+	private int b_3;
+	private int b_4;
+	private int b_5;
+	private int b_6;
+	private int b_7;
+	private int b_8;
+	private int b_9;
+	private int b_10;
+	private int b_11;
+	private int b_12;
+	private int b_13;
+	private int b_14;
+	private int b_15;
+	private int b_16;
+	
+	/**
+	 * 
+	 */
+	
+	private String issueno;
+	
+	/**
+	 * 
+	 */
+	
+	private String issueDate;
+	/**
+	 * 球套号
+	 */
+	private int qNum;
+
+	public int getR_1() {
+		return r_1;
+	}
+
+	public void setR_1(int r_1) {
+		this.r_1 = r_1;
+	}
+
+	public int getR_2() {
+		return r_2;
+	}
+
+	public void setR_2(int r_2) {
+		this.r_2 = r_2;
+	}
+
+	public int getR_3() {
+		return r_3;
+	}
+
+	public void setR_3(int r_3) {
+		this.r_3 = r_3;
+	}
+
+	public int getR_4() {
+		return r_4;
+	}
+
+	public void setR_4(int r_4) {
+		this.r_4 = r_4;
+	}
+
+	public int getR_5() {
+		return r_5;
+	}
+
+	public void setR_5(int r_5) {
+		this.r_5 = r_5;
+	}
+
+	public int getR_6() {
+		return r_6;
+	}
+
+	public void setR_6(int r_6) {
+		this.r_6 = r_6;
+	}
+
+	public int getR_7() {
+		return r_7;
+	}
+
+	public void setR_7(int r_7) {
+		this.r_7 = r_7;
+	}
+
+	public int getR_8() {
+		return r_8;
+	}
+
+	public void setR_8(int r_8) {
+		this.r_8 = r_8;
+	}
+
+	public int getR_9() {
+		return r_9;
+	}
+
+	public void setR_9(int r_9) {
+		this.r_9 = r_9;
+	}
+
+	public int getR_10() {
+		return r_10;
+	}
+
+	public void setR_10(int r_10) {
+		this.r_10 = r_10;
+	}
+
+	public int getR_11() {
+		return r_11;
+	}
+
+	public void setR_11(int r_11) {
+		this.r_11 = r_11;
+	}
+
+	public int getR_12() {
+		return r_12;
+	}
+
+	public void setR_12(int r_12) {
+		this.r_12 = r_12;
+	}
+
+	public int getR_13() {
+		return r_13;
+	}
+
+	public void setR_13(int r_13) {
+		this.r_13 = r_13;
+	}
+
+	public int getR_14() {
+		return r_14;
+	}
+
+	public void setR_14(int r_14) {
+		this.r_14 = r_14;
+	}
+
+	public int getR_15() {
+		return r_15;
+	}
+
+	public void setR_15(int r_15) {
+		this.r_15 = r_15;
+	}
+
+	public int getR_16() {
+		return r_16;
+	}
+
+	public void setR_16(int r_16) {
+		this.r_16 = r_16;
+	}
+
+	public int getR_17() {
+		return r_17;
+	}
+
+	public void setR_17(int r_17) {
+		this.r_17 = r_17;
+	}
+
+	public int getR_18() {
+		return r_18;
+	}
+
+	public void setR_18(int r_18) {
+		this.r_18 = r_18;
+	}
+
+	public int getR_19() {
+		return r_19;
+	}
+
+	public void setR_19(int r_19) {
+		this.r_19 = r_19;
+	}
+
+	public int getR_20() {
+		return r_20;
+	}
+
+	public void setR_20(int r_20) {
+		this.r_20 = r_20;
+	}
+
+	public int getR_21() {
+		return r_21;
+	}
+
+	public void setR_21(int r_21) {
+		this.r_21 = r_21;
+	}
+
+	public int getR_22() {
+		return r_22;
+	}
+
+	public void setR_22(int r_22) {
+		this.r_22 = r_22;
+	}
+
+	public int getR_23() {
+		return r_23;
+	}
+
+	public void setR_23(int r_23) {
+		this.r_23 = r_23;
+	}
+
+	public int getR_24() {
+		return r_24;
+	}
+
+	public void setR_24(int r_24) {
+		this.r_24 = r_24;
+	}
+
+	public int getR_25() {
+		return r_25;
+	}
+
+	public void setR_25(int r_25) {
+		this.r_25 = r_25;
+	}
+
+	public int getR_26() {
+		return r_26;
+	}
+
+	public void setR_26(int r_26) {
+		this.r_26 = r_26;
+	}
+
+	public int getR_27() {
+		return r_27;
+	}
+
+	public void setR_27(int r_27) {
+		this.r_27 = r_27;
+	}
+
+	public int getR_28() {
+		return r_28;
+	}
+
+	public void setR_28(int r_28) {
+		this.r_28 = r_28;
+	}
+
+	public int getR_29() {
+		return r_29;
+	}
+
+	public void setR_29(int r_29) {
+		this.r_29 = r_29;
+	}
+
+	public int getR_30() {
+		return r_30;
+	}
+
+	public void setR_30(int r_30) {
+		this.r_30 = r_30;
+	}
+
+	public int getR_31() {
+		return r_31;
+	}
+
+	public void setR_31(int r_31) {
+		this.r_31 = r_31;
+	}
+
+	public int getR_32() {
+		return r_32;
+	}
+
+	public void setR_32(int r_32) {
+		this.r_32 = r_32;
+	}
+
+	public int getR_33() {
+		return r_33;
+	}
+
+	public void setR_33(int r_33) {
+		this.r_33 = r_33;
+	}
+
+	public int getB_1() {
+		return b_1;
+	}
+
+	public void setB_1(int b_1) {
+		this.b_1 = b_1;
+	}
+
+	public int getB_2() {
+		return b_2;
+	}
+
+	public void setB_2(int b_2) {
+		this.b_2 = b_2;
+	}
+
+	public int getB_3() {
+		return b_3;
+	}
+
+	public void setB_3(int b_3) {
+		this.b_3 = b_3;
+	}
+
+	public int getB_4() {
+		return b_4;
+	}
+
+	public void setB_4(int b_4) {
+		this.b_4 = b_4;
+	}
+
+	public int getB_5() {
+		return b_5;
+	}
+
+	public void setB_5(int b_5) {
+		this.b_5 = b_5;
+	}
+
+	public int getB_6() {
+		return b_6;
+	}
+
+	public void setB_6(int b_6) {
+		this.b_6 = b_6;
+	}
+
+	public int getB_7() {
+		return b_7;
+	}
+
+	public void setB_7(int b_7) {
+		this.b_7 = b_7;
+	}
+
+	public int getB_8() {
+		return b_8;
+	}
+
+	public void setB_8(int b_8) {
+		this.b_8 = b_8;
+	}
+
+	public int getB_9() {
+		return b_9;
+	}
+
+	public void setB_9(int b_9) {
+		this.b_9 = b_9;
+	}
+
+	public int getB_10() {
+		return b_10;
+	}
+
+	public void setB_10(int b_10) {
+		this.b_10 = b_10;
+	}
+
+	public int getB_11() {
+		return b_11;
+	}
+
+	public void setB_11(int b_11) {
+		this.b_11 = b_11;
+	}
+
+	public int getB_12() {
+		return b_12;
+	}
+
+	public void setB_12(int b_12) {
+		this.b_12 = b_12;
+	}
+
+	public int getB_13() {
+		return b_13;
+	}
+
+	public void setB_13(int b_13) {
+		this.b_13 = b_13;
+	}
+
+	public int getB_14() {
+		return b_14;
+	}
+
+	public void setB_14(int b_14) {
+		this.b_14 = b_14;
+	}
+
+	public int getB_15() {
+		return b_15;
+	}
+
+	public void setB_15(int b_15) {
+		this.b_15 = b_15;
+	}
+
+	public int getB_16() {
+		return b_16;
+	}
+
+	public void setB_16(int b_16) {
+		this.b_16 = b_16;
+	}
+
+	public String getIssueno() {
+		return issueno;
+	}
+
+	public void setIssueno(String issueno) {
+		this.issueno = issueno;
+	}
+
+	public String getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(String issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public int getqNum() {
+		return qNum;
+	}
+
+	public void setqNum(int qNum) {
+		this.qNum = qNum;
+	}
+
+	@Override
+	public String toString() {
+		return "HBall [r_1=" + r_1 + ", r_2=" + r_2 + ", r_3=" + r_3 + ", r_4=" + r_4 + ", r_5=" + r_5 + ", r_6=" + r_6
+				+ ", r_7=" + r_7 + ", r_8=" + r_8 + ", r_9=" + r_9 + ", r_10=" + r_10 + ", r_11=" + r_11 + ", r_12="
+				+ r_12 + ", r_13=" + r_13 + ", r_14=" + r_14 + ", r_15=" + r_15 + ", r_16=" + r_16 + ", r_17=" + r_17
+				+ ", r_18=" + r_18 + ", r_19=" + r_19 + ", r_20=" + r_20 + ", r_21=" + r_21 + ", r_22=" + r_22
+				+ ", r_23=" + r_23 + ", r_24=" + r_24 + ", r_25=" + r_25 + ", r_26=" + r_26 + ", r_27=" + r_27
+				+ ", r_28=" + r_28 + ", r_29=" + r_29 + ", r_30=" + r_30 + ", r_31=" + r_31 + ", r_32=" + r_32
+				+ ", r_33=" + r_33 + ", b_1=" + b_1 + ", b_2=" + b_2 + ", b_3=" + b_3 + ", b_4=" + b_4 + ", b_5=" + b_5
+				+ ", b_6=" + b_6 + ", b_7=" + b_7 + ", b_8=" + b_8 + ", b_9=" + b_9 + ", b_10=" + b_10 + ", b_11="
+				+ b_11 + ", b_12=" + b_12 + ", b_13=" + b_13 + ", b_14=" + b_14 + ", b_15=" + b_15 + ", b_16=" + b_16
+				+ ", issueno=" + issueno + ", issueDate=" + issueDate + ", qNum=" + qNum + "]";
+	}
+	
+	
+	
+}
